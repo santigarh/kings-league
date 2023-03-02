44a5a3a2406bcc01c36cb75536e5f4ec1b1f8dc6
Santiago <santigarh.77@gmail.com>
2023-03-02 17:15:50 -0500

    add service for leaderbaord

--- src/services/leaderboard.js
+++ src/services/leaderboard.js
@@ -9,3 +9,13 @@ export const findLeaderboardBy = async ({ teamId }) => {
     return []
   }
 }
+
+export const getLeaderboard = async () => {
+  try {
+    const response = await fetch(`${apiURL}/leaderboard`)
+    const leaderboard = await response.json()
+    return leaderboard
+  } catch (e) {
+    return []
+  }
+}
