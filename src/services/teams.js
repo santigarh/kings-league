bb0a71bd87dab001682e9b5675a64fc80bbcfbe9
Santiago <santigarh.77@gmail.com>
2023-03-02 17:55:21 -0500

    colores config

--- src/services/teams.js
+++ src/services/teams.js
@@ -1,26 +1,34 @@
-import { apiURL } from './config'
+import { apiURL } from './config.js'
 
 export const getAllTeams = async () => {
   try {
     const response = await fetch(`${apiURL}/teams`)
-
     const teams = await response.json()
-
     return teams
   } catch (e) {
-    //enviar error en reporte de errores
+    // enviar el error a un servicio de reporte de errores
     return []
   }
 }
 
-export const getPlayerTwelveFor = async ({ teamId }) => {
+export const getPlayersTwelveFor = async ({ teamId }) => {
   try {
-    const response = await fetch(`${apiURL}/teams/${teamId}/player-12`)
-    const teams = await response.json()
-
-    return teams
+    const response = await fetch(`${apiURL}/teams/${teamId}/players-12`)
+    const players = await response.json()
+    return players
   } catch (e) {
-    //enviar error en reporte de errores
+    // enviar el error a un servicio de reporte de errores
+    return []
+  }
+}
+
+export const getAllPlayersTwelve = async () => {
+  try {
+    const response = await fetch(`${apiURL}/players-12`)
+    const players = await response.json()
+    return players
+  } catch (e) {
+    // enviar el error a un servicio de reporte de errores
     return []
   }
 }
